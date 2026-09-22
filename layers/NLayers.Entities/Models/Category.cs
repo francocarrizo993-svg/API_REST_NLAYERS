0aa92c499f13dc15a0bcd3adcf71e925667aa916
Mauricio Saavedra <mauricio.young@mi.unc.edu.ar>
2026-09-21 21:27:06 -0300

    feat: add Category entity

---
+++ layers/NLayers.Entities/Models/Category.cs
@@ -0,0 +1,10 @@
+namespace NLayers.Entities.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string? Description { get; set; }
+}

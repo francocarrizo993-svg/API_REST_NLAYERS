37c57710672172c94e094e0651ee220dd313b841
Leandro Carranza <leandro.carranza@mi.unc.edu.ar>
2026-09-22 18:22:06 -0300

    Integrar Category: DI (Singleton en CategoryStore), ApplicationPart y MapControllers en Program.cs

--- backend/NLayers.Api/Program.cs
+++ backend/NLayers.Api/Program.cs
@@ -1,8 +1,18 @@
+using NLayers.DataAccess.Stores;
+using NLayers.BusinessLogic.Managers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers()
+    .AddApplicationPart(typeof(NLayers.Presentation.Controllers.CategoryController).Assembly);
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Inyección de dependencias (por ahora sin interfaces)
+builder.Services.AddSingleton<CategoryStore>();
+builder.Services.AddScoped<CategoryManager>();
 
 var app = builder.Build();
 
@@ -14,28 +24,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}

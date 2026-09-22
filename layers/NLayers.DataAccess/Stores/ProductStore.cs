cba7114f4ebbb05532133209efef97f392d632eb
franco.carrizo <franco.carrizo.993@mi.unc.edu.ar>
2026-09-22 20:59:32 -0300

    feat: add Product entity with N-Layers architecture

---
+++ layers/NLayers.DataAccess/Stores/ProductStore.cs
@@ -0,0 +1,11 @@
+﻿using NLayers.DataAccess.Stores.Sql;
+using NLayers.Entities.Models;
+
+namespace NLayers.DataAccess.Stores;
+
+public class ProductStore : BaseSqlStore<Product>
+{
+    public ProductStore(AppDbContext context) : base(context)
+    {
+    }
+}
